2d04682fe852eef3755123a82309721b17219aea
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-02 15:19:52 -0300

    fix: omitted column name
    The column name was previously changed, but the type wasn't.

--- src/types/testTypes.ts
+++ src/types/testTypes.ts
@@ -2,7 +2,7 @@ import { Test } from "@prisma/client";
 
 export { Test };
 export type TestCreationData = Omit<Test, "id">;
-export type TestRequestData = Omit<TestCreationData, "teachersDisciplineId"> & {
+export type TestRequestData = Omit<TestCreationData, "teacherDisciplineId"> & {
   disciplineId: number;
   teacherId: number;
 };
