b651de890ab0956883068e12507a0ccdadcd471a
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-02 17:48:39 -0300

    test: add token factory to user
    This token will be used to test authenticated routes.

--- tests/factories/userFactory.ts
+++ tests/factories/userFactory.ts
@@ -1,4 +1,8 @@
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export function signupInfos() {
   const password: string = faker.internet.password();
@@ -6,5 +10,11 @@ export function signupInfos() {
     email: faker.internet.email(),
     password: password,
     confirmPassword: password,
-  }
+  };
+}
+
+export function token() {
+  const secretKey: string = process.env.JWT_SECRET_KEY || "secret";
+  const token: string = jwt.sign({ userId: 1 }, secretKey);
+  return token;
 }
