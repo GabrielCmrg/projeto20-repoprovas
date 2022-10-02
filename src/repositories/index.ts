46a700238f597e479dd59a527c7c00c71b6ac27f
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-02 12:10:05 -0300

    chore: add new repositories to bundler

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,2 +1,5 @@
 export * as userRepository from "./userRepository";
 export * as testRepository from "./testRepository";
+export * as categoryRepository from "./categoryRepository";
+export * as disciplineRepository from "./disciplineRepository";
+export * as teacherRepository from "./teacherRepository";
