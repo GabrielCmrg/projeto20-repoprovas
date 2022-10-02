a9c6b3b2274fa34a5d3cd63c139b495c3a68306d
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-02 13:25:01 -0300

    refactor: change function name
    The change of the name is because its easier to understand what the
    result represents.

--- src/repositories/teacherDisciplineRepository.ts
+++ src/repositories/teacherDisciplineRepository.ts
@@ -1,13 +1,13 @@
 import { client } from "../config/database";
 import { TeachersDisciplines } from "../types/teacherDisciplineTypes";
 
-export async function getTeacherDiscipline(
+export async function getRelation(
   disciplineId: number,
   teacherId: number
 ): Promise<TeachersDisciplines | null> {
-  const teacherDiscipline: TeachersDisciplines | null =
+  const relation: TeachersDisciplines | null =
     await client.teachersDisciplines.findUnique({
       where: { teacherId_disciplineId: { teacherId, disciplineId } },
     });
-  return teacherDiscipline;
+  return relation;
 }
