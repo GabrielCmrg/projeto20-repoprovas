2afbf76f5b7091bdd4247393f0301e258a3672d2
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-02 20:47:21 -0300

    test: change test to avoid re using the route

--- tests/signup.test.ts
+++ tests/signup.test.ts
@@ -1,46 +1,44 @@
-import supertest from 'supertest';
-import Joi from 'joi';
+import supertest from "supertest";
+import Joi from "joi";
 
-import app from '../src/app';
-import { client } from '../src/config/database';
-import * as user from './factories/userFactory';
+import app from "../src/app";
+import { client } from "../src/config/database";
+import * as user from "./factories/userFactory";
 
-describe('Testing sing-up', () => {
-
+describe("Testing sing-up", () => {
   beforeEach(async () => {
     await client.$executeRaw`TRUNCATE TABLE users RESTART IDENTITY`;
   });
 
-  it('Should return status code 201 and the created user', async () => {
+  it("Should return status code 201 and the created user", async () => {
     const returnSchema = Joi.object({
       id: Joi.number().integer().greater(0).required(),
       email: Joi.string().email().required(),
     });
     const body = user.signupInfos();
-    const result = await supertest(app).post('/signup').send(body);
+    const result = await supertest(app).post("/signup").send(body);
     const validation = returnSchema.validate(result.body);
     expect(result.status).toBe(201);
     expect(validation.error).toBeFalsy();
   });
 
-  it('Should return status code 422 when password is different from confirmation', async () => {
+  it("Should return status code 422 when password is different from confirmation", async () => {
     const body = user.signupInfos();
-    body.confirmPassword = 'differentPassword';
-    const result = await supertest(app).post('/signup').send(body);
+    body.confirmPassword = "differentPassword";
+    const result = await supertest(app).post("/signup").send(body);
     expect(result.status).toBe(422);
   });
 
-  it('Should return status code 409 when email is in use', async () => {
+  it("Should return status code 409 when email is in use", async () => {
     const body = user.signupInfos();
-    const preTest = await supertest(app).post('/signup').send(body);
-    expect(preTest.status).toBe(201);
-    expect(preTest.body.email).toBe(body.email);
-    const result = await supertest(app).post('/signup').send(body);
+    await client.user.create({
+      data: { email: body.email, password: body.password },
+    });
+    const result = await supertest(app).post("/signup").send(body);
     expect(result.status).toBe(409);
   });
 
   afterAll(async () => {
     await client.$disconnect();
-  })
-
+  });
 });
