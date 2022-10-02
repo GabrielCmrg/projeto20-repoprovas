fdd270ee923ac28603caf007ee502fb83d1e3326
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-02 20:40:07 -0300

    test: change tests to have only 1 preparation
    Since the goal is not to test the signup but the login, i changed the
    preparation of each test to be unique, and use this preparation in all
    tests.

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -1,57 +1,52 @@
-import supertest from 'supertest';
-import Joi from 'joi';
+import supertest from "supertest";
+import Joi from "joi";
+import bcrypt from "bcrypt";
 
-import app from '../src/app';
-import { client } from '../src/config/database';
-import * as user from './factories/userFactory';
+import app from "../src/app";
+import { client } from "../src/config/database";
+import * as user from "./factories/userFactory";
 
-describe('Testing login', () => {
+const createAccount = user.signupInfos();
 
-  beforeEach(async () => {
-    await client.$executeRaw`TRUNCATE TABLE users RESTART IDENTITY`;
+describe("Testing login", () => {
+  beforeAll(async () => {
+    const hashPassword = bcrypt.hashSync(createAccount.password, 10);
+    await client.user.create({
+      data: { email: createAccount.email, password: hashPassword },
+    });
   });
 
-  it('Should return status code 201 and token given right credentials', async () => {
-    const createAccount = user.signupInfos();
-    const preTest = await supertest(app).post('/signup').send(createAccount);
-    expect(preTest.status).toBe(201);
+  it("Should return status code 201 and token given right credentials", async () => {
     const returningSchema = Joi.object({ token: Joi.string().required() });
     const body = {
       email: createAccount.email,
       password: createAccount.password,
-    }
-    const result = await supertest(app).post('/login').send(body);
-    expect(result.status).toBe(201);
+    };
+    const result = await supertest(app).post("/login").send(body);
     const validation = returningSchema.validate(result.body);
+    expect(result.status).toBe(201);
     expect(validation.error).toBeFalsy();
   });
 
-  it('Should return status code 401 if wrong email is sent', async () => {
-    const createAccount = user.signupInfos();
-    const preTest = await supertest(app).post('/signup').send(createAccount);
-    expect(preTest.status).toBe(201);
+  it("Should return status code 401 if wrong email is sent", async () => {
     const body = {
-      email: 'babaloo@gmail.com',
+      email: "babaloo@gmail.com",
       password: createAccount.password,
-    }
-    const result = await supertest(app).post('/login').send(body);
+    };
+    const result = await supertest(app).post("/login").send(body);
     expect(result.status).toBe(401);
   });
 
-  it('Should return status code 401 if wrong password is sent', async () => {
-    const createAccount = user.signupInfos();
-    const preTest = await supertest(app).post('/signup').send(createAccount);
-    expect(preTest.status).toBe(201);
+  it("Should return status code 401 if wrong password is sent", async () => {
     const body = {
       email: createAccount.email,
-      password: 'wrongPassword',
-    }
-    const result = await supertest(app).post('/login').send(body);
+      password: "wrongPassword",
+    };
+    const result = await supertest(app).post("/login").send(body);
     expect(result.status).toBe(401);
   });
 
   afterAll(async () => {
     await client.$disconnect();
-  })
-
+  });
 });
