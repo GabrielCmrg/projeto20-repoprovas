100ddae4710cd977ed2a0700a6671f89b3bbeb2b
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-03 18:15:38 -0300

    feat: create a type conversion function
    The function converts discipline types to disciplineData types.

--- src/types/disciplineTypes.ts
+++ src/types/disciplineTypes.ts
@@ -3,6 +3,7 @@ import { Discipline as DisciplineModel } from "@prisma/client";
 import { CategoryData } from "./categoryTypes";
 import { Term } from "./termTypes";
 import { TeacherDiscipline } from "./teacherDisciplineTypes";
+import { groupTestsByCategories } from "../services/testService";
 
 export type Discipline = DisciplineModel & {
   term?: Term;
@@ -12,3 +13,11 @@ export type DisciplineCreationData = Omit<DisciplineModel, "id">;
 export type DisciplineData = Omit<DisciplineModel, "termId"> & {
   categories: CategoryData[];
 };
+
+export function buildDisciplineData(discipline: Discipline): DisciplineData {
+  return {
+    id: discipline.id,
+    name: discipline.name,
+    categories: groupTestsByCategories(discipline.teachers!),
+  };
+}
