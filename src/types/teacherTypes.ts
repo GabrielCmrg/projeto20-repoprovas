75ba1a6066c0a00eeeb642007524c54cffbf4578
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-04 17:07:35 -0300

    feat: add teacher type with nested tests

--- src/types/teacherTypes.ts
+++ src/types/teacherTypes.ts
@@ -1,8 +1,13 @@
 import { Teacher as TeacherModel } from "@prisma/client";
 
+import { CategoryData } from "./categoryTypes";
 import { TeacherDiscipline } from "./teacherDisciplineTypes";
+import { TestDisciplineData } from "./testTypes";
 
 export type Teacher = TeacherModel & {
   disciplines?: TeacherDiscipline[];
 };
 export type TeacherCreationData = Omit<TeacherModel, "id">;
+export type TeacherData = TeacherModel & {
+  categories: CategoryData<TestDisciplineData>[];
+};
