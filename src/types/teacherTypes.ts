8651cd2cf724355ba2a3711901bccd75cfe27ab4
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-04 17:47:18 -0300

    feat: add function to convert Teacher to TeacherData

--- src/types/teacherTypes.ts
+++ src/types/teacherTypes.ts
@@ -2,7 +2,8 @@ import { Teacher as TeacherModel } from "@prisma/client";
 
 import { CategoryData } from "./categoryTypes";
 import { TeacherDiscipline } from "./teacherDisciplineTypes";
-import { TestDisciplineData } from "./testTypes";
+import { Test, TestDisciplineData, buildTestDisciplineData } from "./testTypes";
+import { groupTestsByCategories } from "../services/testService";
 
 export type Teacher = TeacherModel & {
   disciplines?: TeacherDiscipline[];
@@ -11,3 +12,22 @@ export type TeacherCreationData = Omit<TeacherModel, "id">;
 export type TeacherData = TeacherModel & {
   categories: CategoryData<TestDisciplineData>[];
 };
+
+export function buildTeacherData(teacher: Teacher): TeacherData {
+  return {
+    id: teacher.id,
+    name: teacher.name,
+    categories: groupTestsByCategories(teacher.disciplines!).map(
+      (category: CategoryData<Test>): CategoryData<TestDisciplineData> => {
+        const converted: TestDisciplineData[] = category.tests!.map(
+          buildTestDisciplineData
+        );
+        return {
+          id: category.id,
+          name: category.name,
+          tests: converted,
+        };
+      }
+    ),
+  };
+}
