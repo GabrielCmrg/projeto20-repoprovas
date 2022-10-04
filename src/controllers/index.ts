6c44fb1f0d349afce50aee709567b3b474ac176e
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-04 17:53:46 -0300

    feat: create teacher controller
    This controller will get all teachers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,4 @@
 export * as userController from "./userController";
 export * as testController from "./testController";
 export * as termController from "./termController";
+export * as teacherController from "./teacherController";
