e0e43525e9c21ab759b922a140f66b77a1251a89
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-04 17:52:01 -0300

    fix: add teacher service to bundler

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,4 @@
 export * as userService from "./userService";
 export * as testService from "./testService";
 export * as termService from "./termService";
+export * as teacherService from "./teacherService";
